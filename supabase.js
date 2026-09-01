52b8ede8d164c727a2d301de3b44ffdc27d0095f
detechstudios-web <detechstudios@gmail.com>
2026-09-01 11:42:59 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,12 +1,15 @@
-// ============================================
+// ========================================
 // KABARU WARD FOOTBALL
 // Supabase Connection
-// ============================================
+// ========================================
 
 const SUPABASE_URL = "https://aitcfhalfkdlitaefhfo.supabase.co";
 
+// Use your SUPABASE PUBLISHABLE KEY here.
+// DO NOT use the secret key in this file.
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_NyK1UAffJLTxXwyGxD89Ww_O2J-iPhl";
 
+// Create Supabase client
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
